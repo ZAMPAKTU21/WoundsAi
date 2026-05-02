687a2c6b5e347e543c98259c084b71a5203be3fa
ZAMPAKTU21 <jhcurayag@gmail.com>
2026-05-03 01:49:13 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'WoundScanAi';
+
+// Add every file your app needs here
+// Note: Ensure these paths match your actual folder structure!
+const ASSETS = [
+  './',
+  './index.html',
+  './mode_files/model.json',
+  './mode_files/weights.bin',
+  './mode_files/metadata.json'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
